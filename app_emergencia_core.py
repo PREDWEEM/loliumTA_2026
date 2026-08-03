9cb2ce9df6cdf5f67046003065e537eca735a422
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-08-03 12:22:15 -0300

    Update app_emergencia_core.py

--- app_emergencia_core.py
+++ app_emergencia_core.py
@@ -675,7 +675,7 @@ else:
     st.sidebar.caption("Motor predictivo Lolium — Tres Arroyos")
 
 st.sidebar.markdown("## ⚙️ 2. Fisiología y Logística")
-umbral_er = st.sidebar.slider("Umbral Alerta Temprana", 0.001, 0.80, 0.001)
+umbral_er = st.sidebar.slider("Umbral Alerta Temprana", 0.0001, 0.80, 0.0001)
 
 st.sidebar.markdown("**Ruptura de Dormición Estival (Escudo)**")
 umbral_termoinhibicion = st.sidebar.number_input("Umbral Termoinhibición (°C)", 15.0, 35.0, 24.0, 0.5)
